dd781c4c46c2d02d760542c6917cca5f048ab186
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-04-09 22:49:24 +0200

    Clean up

--- __tests__/sample_test.js
+++ __tests__/sample_test.js
@@ -1,4 +1,5 @@
 /* eslint-env jest */
+
 const { ethers } = require('hardhat')
 
 describe('Greeter', function () {
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,19 +5,6 @@ require('@nomiclabs/hardhat-waffle')
 require('hardhat-gas-reporter')
 require('solidity-coverage')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -26,7 +13,8 @@ module.exports = {
   paths: {
     artifacts: './artifacts',
     sources: './contracts',
-    tests: './test'
+    cache: './cache',
+    tests: './__test__'
   },
   networks: {
     ropsten: {
@@ -36,6 +24,9 @@ module.exports = {
     },
     hardhat: {
       chainId: 1337
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545'
     }
   },
   gasReporter: {
