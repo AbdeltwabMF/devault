513c5fc42cae8c7fec905c7ca8833eef3d0e24d0
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-04-29 22:30:01 +0200

    Add new workflows

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 # install long term support node.js
-source "$HOME"/.config/nvm/nvm.sh
-nvm install --lts
+if [[ -f "$HOME"/.config/nvm/nvm.sh ]]; then
+	source "$HOME"/.config/nvm/nvm.sh
+	nvm install --lts
+fi
 
 # compile the smart contracts
 npx hardhat compile
