b3a428e178b3893fb9286d68ebabf58bdba84ad3
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-05-22 03:19:51 +0200

    Handle capturing and uploading file

--- components/ReadData/ReadData.js
+++ components/ReadData/ReadData.js
@@ -4,12 +4,12 @@ import styles from './ReadData.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
-export default function ReadData ({ choosenFile }) {
+export default function ReadData ({ captureFile, uploadFile }) {
   return (
     <>
-      <Form className={styles.main}>
+      <Form className={styles.main} onSubmit={uploadFile}>
         <Form.Group controlId='formFile'>
-          <Form.Control type='file' className={styles.control} required name='file' onChange={choosenFile} /* isInvalid={invalidType} */ />
+          <Form.Control type='file' className={styles.control} required name='file' onChange={captureFile} /* isInvalid={invalidType} */ />
         </Form.Group>
         <Button variant='primary' type='submit' className={styles.button}>Upload <FontAwesomeIcon icon={faPlus} size='xl' fixedWidth className={styles.icon} /></Button>
       </Form>
--- pages/vault.js
+++ pages/vault.js
@@ -18,6 +18,9 @@ import styles from '../styles/Vault.module.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { create, CID } from 'ipfs-http-client'
+
+const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 export default function Vault () {
   const [account, setAccount] = useState('')
@@ -27,6 +30,10 @@ export default function Vault () {
   const [type, setType] = useState('')
   const [name, setName] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [Cid, setCid] = useState('')
+  const [path, setPath] = useState('')
+  const [smartContract, setSmartContract] = useState('')
+  const [smartContractAddress, setSmartContractAddress] = useState('')
 
   /**
   * @description Connecting to metamask.
@@ -39,35 +46,71 @@ export default function Vault () {
   * @param {jsonInterface} contract.abi - Object: The json interface for the contract to instantiate
   * @returns {object} - The contract instance with all its methods and events.
   */
-  const smartContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
-  const smartContract = new web3.eth.Contract(Storage.abi, smartContractAddress)
+  const instantiateContract = async () => {
+    setSmartContractAddress('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+    setSmartContract(new web3.eth.Contract(Storage.abi, smartContractAddress))
 
-  /**
+    /**
   * @description Getting the Owner Files Count.
   */
-  const getFilesCount = async () => {
     const filesCount = await smartContract.methods.getOwnerFilesCount().call()
     console.log(filesCount)
     setFilesCount(filesCount)
+    const files = []
+    for (let i = 0; i < filesCount; i++) {
+      const file = await smartContract.methods.getOwnerFile(i).call()
+      files.push(file)
+    }
+    console.log(files)
+    setFiles(files)
   }
-  getFilesCount()
+  console.log(instantiateContract)
 
   /**
   * @description Reading contents of files (or raw data buffers) stored on the user's computer.
   */
-  const handleFileReader = (e) => {
+  const captureFile = async (e) => {
+    e.preventDefault()
     // Read only the first file
-    const file = e.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
-
-    reader.onloadend = (e) => {
-      // The file's text will be printed here
-      console.log(e.target.result)
-      setBuffer(Buffer.from(reader.result))
-      setType(file.type)
-      setName(file.name)
+    try {
+      const file = e.target.files[0]
+      const reader = new window.FileReader()
+      reader.onload = async (e) => {
+        console.log(e.target.result)
+        setBuffer('File Buffer: ', Buffer.from(reader.result))
+        setType(file.type)
+        setName(file.name)
+      }
+      reader.readAsArrayBuffer(file)
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  const uploadFile = async () => {
+    const result = await ipfs.add(buffer, (err, res) => {
+      if (err) {
+        console.log('Error IPFS: ', err)
+      } else {
+        console.log('IPFS respond: ', res)
+        setCid(res[0].hash)
+        setPath(res[0].path)
+        setIsLoading(true)
+
+        if (type === '') { setType('none') }
+        smartContract.methods.storeFile(name, res[0].size, type, path).send({ from: account }).on('TransactionHash', (hash) => {
+          console.log(hash)
+          setIsLoading(false)
+          setType('')
+          setName('')
+          // window.location.reload()
+        }).on('error', (error) => {
+          console.log(error)
+          setIsLoading(false)
+        })
+      }
+    })
+    console.log(result)
   }
 
   return (
@@ -75,7 +118,9 @@ export default function Vault () {
       <Container className={styles.container}>
         <Row>
           <Col xs={12} className={styles.readData}>
-            <ReadData choosenFile={handleFileReader} />
+            {isLoading
+              ? <div id='loader' className='text-center mt-5'><p>Loading...</p></div>
+              : <ReadData captureFile={captureFile} uploadFile={uploadFile} />}
             <hr className={styles.devider} />
           </Col>
           <Col xs={12}>
@@ -83,6 +128,11 @@ export default function Vault () {
           </Col>
         </Row>
       </Container>
+      <div>
+        {!ipfs && (
+          <p>Oh oh, Not connected to IPFS. Checkout out the logs for errors</p>
+        )}
+      </div>
     </>
   )
 }
