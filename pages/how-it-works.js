93262abcc686e00c8867596f210d5aa2bca2821e
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-06-11 01:13:58 +0200

    Fix typo

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -1,11 +1,9 @@
 import styles from '../styles/HowItWorks.module.css'
-import Image from 'next/image'
 
 export default function HowItWorks () {
   return (
     <>
       <div className={styles.main}>
-        <Image src='/favicon.svg' alt='Decentralized Vault' width={128} height={128} />
       </div>
     </>
   )
