42248923cc5910d44ac96181289030fc6e3eaacd
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-06-11 02:18:31 +0200

    Fix dependencies issues

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { Vim, Artixlinux, Github, Telegram, ReactJs } from '@icons-pack/react-simple-icons/'
+// import { Vim, Artixlinux, Github, Telegram, ReactJs } from '@icons-pack/react-simple-icons/'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseLock } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
@@ -12,24 +12,24 @@ export default function Footer () {
           <div className={'col col-lg-4 col-12 ' + `${styles.col1}`}>
             <Link href='https://github.com/abdeltwabmf/decentralized-evault'>
               <a className={styles.social}>
-                <Github color='teal' className={styles.socialIcon} />
+                {/* <Github color='teal' className={styles.socialIcon} /> */}
                 <span className={styles.labelLink}>View Source</span>
               </a>
             </Link>
             <Link href='https://t.me/+OeH3hX00HqxmZDc8'>
               <a className={styles.social}>
-                <Telegram color='teal' className={styles.socialIcon} />
+                {/* <Telegram color='teal' className={styles.socialIcon} /> */}
                 <span className={styles.labelLink}>Join Telegram</span>
               </a>
             </Link>
           </div>
           <div className={'col col-lg-4 col-12 ' + `${styles.col2}`}>
             <span className={styles.label}>Built with</span>
-            <Vim color='teal' size={19} className={styles.toolsIcon} />
+            {/* <Vim color='teal' size={19} className={styles.toolsIcon} /> */}
             <span className={styles.label}>&amp;</span>
-            <ReactJs color='teal' size={19} className={styles.toolsIcon} />
+            {/* <ReactJs color='teal' size={19} className={styles.toolsIcon} /> */}
             <span className={styles.label}>on</span>
-            <Artixlinux color='teal' size={19} className={styles.toolsIcon} />
+            {/* <Artixlinux color='teal' size={19} className={styles.toolsIcon} /> */}
           </div>
           <div className={'col col-lg-4 col-12 ' + `${styles.col3}`}>
             &copy; {new Date().getFullYear()}
