de61784ef4498aa5cf0873902cad13a67ef0e9bc
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-06-28 15:28:55 +0200

    Update package.json scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,27 +10,25 @@ if [[ -f ./scripts/logger.sh ]]; then
 	source ./scripts/logger.sh
 fi
 
+# Install long term support node.js to match the version supported by hardhat.
+# this require you to install nvm first
+# and put it under the following path: $HOME/.config/nvm/
 install_nvm() {
 	export XDG_CONFIG_HOME=$HOME/.config
 	unset npm_config_prefix
 	curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+	if [[ -f "$HOME"/.config/nvm/nvm.sh ]]; then
+		source "$HOME"/.config/nvm/nvm.sh
+		info "NVM is installed and loaded"
+		nvm install --lts >/dev/null 2>&1
+		info "Now using node $(node -v) (npm $(npm -v))"
+	else
+		error "Couldn't install NVM, check the network"
+		info "Install nvm manually first, so that it can install the supported version of node."
+		exit 1
+	fi
 }
-
-# install long term support node.js to match the version supported by hardhat.
-# this require you to install nvm first
-# and put it under the following path: $HOME/.config/nvm/
-install_nvm >/dev/null 2>&1
-
-if [[ -f "$HOME"/.config/nvm/nvm.sh ]]; then
-	source "$HOME"/.config/nvm/nvm.sh
-	info "NVM is installed and loaded"
-	nvm install --lts >/dev/null 2>&1
-	info "Now using node $(node -v) (npm $(npm -v))"
-else
-	error "Couldn't install NVM, check the network"
-	info "Install nvm manually first, so that it can install the supported version of node."
-	exit 1
-fi
 
 start_services() {
 	# start local blockchain node
@@ -53,6 +51,16 @@ compile_smart_contract() {
 	info "$(npx hardhat compile)"
 }
 
+decorize_address() {
+	printf "%b" "${BBLUE}${smart_contract_address}${CYAN}"
+}
+
+# deploy the smart contracts to the specified network
+deploy_to_network() {
+	info "Deploying the smart contracts to ${network} network..."
+	smart_contract_address=$(npx hardhat run ./scripts/deploy.js --network "$network" 2>/dev/null)
+}
+
 # replace the address of the smart contract with the deployed address in the app root
 do_final() {
 	sed -i "s/contractAddress = .*$/contractAddress = '$smart_contract_address'/" ./pages/_app.js
@@ -60,50 +68,66 @@ do_final() {
 
 compile_smart_contract
 
-network="localhost"
-case "$1" in
-	"mainnet")
-		network="mainnet" ;;
-	"rinkeby")
-		network="rinkeby" ;;
-	"ropsten")
-		network="ropsten" ;;
-	"kovan")
-		network="kovan" ;;
-esac
-
+network=""
 smart_contract_address=""
 
-decorize_address() {
-	printf "%b" "${BBLUE}${smart_contract_address}${CYAN}"
+check_network() {
+	case "$1" in
+		"mainnet")
+			network="mainnet" ;;
+		"rinkeby")
+			network="rinkeby" ;;
+		"ropsten")
+			network="ropsten" ;;
+		"kovan")
+			network="kovan" ;;
+		*)
+			network="localhost" ;;
+	esac
 }
 
-# deploy the smart contracts to the specified network
-deploy() {
-	info "Deploying the smart contracts to ${network} network..."
-	smart_contract_address=$(npx hardhat run ./scripts/deploy.js --network "$network" 2>/dev/null)
-}
-
-if [[ "$network" != "localhost" ]]; then
-	if (( $2 == 1 )); then
-		deploy
-		info "A new smart contract address: $(decorize_address) is cached"
-		sed -i "s/SMART_CONTRACT_ADDRESS_ROPSTEN.*$/SMART_CONTRACT_ADDRESS_ROPSTEN=$smart_contract_address/" ./.env
+main() {
+	if [[ "$network" != "localhost" ]]; then
+		if (( $2 == 1 )); then
+			deploy_to_network
+			info "A new smart contract address: $(decorize_address) is cached"
+			sed -i "s/SMART_CONTRACT_ADDRESS_ROPSTEN.*$/SMART_CONTRACT_ADDRESS_ROPSTEN=$smart_contract_address/" ./.env
+		else
+			smart_contract_address=$(grep "SMART_CONTRACT_ADDRESS_ROPSTEN" ./.env | cut -d "=" -f 2)
+			info "The cached smart contract address: $(decorize_address) is retrieved"
+		fi
+		do_final
 	else
-		smart_contract_address=$(grep "SMART_CONTRACT_ADDRESS_ROPSTEN" ./.env | cut -d "=" -f 2)
-		info "The cached smart contract address: $(decorize_address) is retrieved"
+		deploy_to_network
+		if [[ $smart_contract_address == "" ]]; then
+			install_nvm >/dev/null 2>&1
+			start_services
+		fi
+		while [[ $smart_contract_address == "" ]]; do
+			error "Unable to deploy the smart contracts to $network network"
+			warn "Please check your node is up and running and try again"
+			info "Retrying in 5 seconds..."
+			sleep 5s
+			deploy_to_network
+		done
+		info "The smart contracts: $(decorize_address) deployed successfully to ${network} network"
+		do_final
 	fi
-	do_final
+}
+
+# parse the command line arguments
+if [[ $# -eq 0 ]]; then
+	error "No network specified"
+	info "Usage: $0 <network> [redeploy]"
+	exit 1
+elif [[ $# -eq 1 ]]; then
+	check_network "$1"
+	main "$network" 0
+elif [[ $# -eq 2 ]]; then
+	check_network "$1"
+	main "$network" "$2"
 else
-	start_services
-	deploy
-	while [[ $smart_contract_address == "" ]]; do
-		error "Unable to deploy the smart contracts to $network network"
-		warn "Please check your node is up and running and try again"
-		info "Retrying in 5 seconds..."
-		sleep 5s
-		deploy
-	done
-	info "The smart contracts: $(decorize_address) deployed successfully to ${network} network"
-	do_final
+	error "Too many arguments"
+	info "Usage: $0 <network> [redeploy]"
+	exit 1
 fi
