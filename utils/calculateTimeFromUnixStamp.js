45113e4b35cab79aa81e849fdece046abab6f9ee
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-06 02:30:50 +0200

    Fix date

--- utils/calculateTimeFromUnixStamp.js
+++ utils/calculateTimeFromUnixStamp.js
@@ -2,26 +2,21 @@
     * @param {number} timestamp - The unix timestamp to format
     * @returns {string} - The formatted timestamp
     */
+
+const convertTo12HourTime = (hour, min) => {
+  const ampm = hour >= 12 ? 'PM' : 'AM'
+  const hh = hour % 12 || 12
+  return hh + ':' + (min < 10 ? '0' : '') + min + ' ' + ampm
+}
+
 const calculateTimeFromUnixStamp = (unixTimestamp) => {
   const date = new Date(unixTimestamp * 1000)
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
   const year = date.getFullYear()
-  const month = months[date.getMonth()]
+  const month = date.getMonth() + 1
   const day = date.getDate()
-  let hour = date.getHours() - 2
-  if (hour < 10) {
-    hour = '0' + hour
-  }
-  let min = date.getMinutes()
-  if (min < 10) {
-    min = '0' + min
-  }
-
-  let sec = date.getSeconds()
-  if (sec < 10) {
-    sec = '0' + sec
-  }
-  const time = day + ' ' + month + ' ' + hour + ':' + min + ':' + sec + ' ' + year
+  const hour = date.getHours()
+  const min = date.getMinutes()
+  const time = day + '/' + month + '/' + year + ' ' + convertTo12HourTime(hour, min)
   return time
 }
 
