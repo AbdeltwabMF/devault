3aa291167ea7bc315fa89e582f19a06eeeccb441
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-08 03:21:32 +0200

    Add pagination

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -12,7 +12,7 @@ contract Storage {
      * @dev Map the owner address to array of files
      */
     mapping(address => File[]) private _ownerToFiles;
-    mapping(string => uint256) private _hashToIndex;
+    mapping(address => mapping(string => uint256)) private _hashToIndex;
 
     struct File {
         string name;
@@ -59,7 +59,7 @@ contract Storage {
         uint256 _size,
         string memory _mimeType,
         string memory _hash
-    ) public returns (bool) {
+    ) public {
         require(bytes(_name).length > 0, "Name cannot be empty");
         require(
             bytes(_name).length <= 512,
@@ -71,8 +71,8 @@ contract Storage {
 
         require(bytes(_mimeType).length > 0, "Mime type cannot be empty");
         require(
-            bytes(_mimeType).length <= 128,
-            "Mime type cannot be longer than 128 bytes"
+            bytes(_mimeType).length <= 512,
+            "Mime type cannot be longer than 512 bytes"
         );
 
         require(bytes(_hash).length > 0, "Hash cannot be empty");
@@ -85,21 +85,19 @@ contract Storage {
             block.timestamp
         );
 
-        if (_hashToIndex[_hash] == 0) {
-            _hashToIndex[_hash] = _ownerToFiles[msg.sender].length + 1;
+        // check if the file's hash is already present in the hashes array
+        if (_hashToIndex[msg.sender][_hash] == 0) {
             _ownerToFiles[msg.sender].push(newFile);
+            _hashToIndex[msg.sender][_hash] = _ownerToFiles[msg.sender].length;
 
             emit FileStored(_name, _size, _mimeType, _hash, block.timestamp);
-
-            return true;
         }
-        return false;
     }
 
     /**
      * @dev Get the files count of the owner
      */
-    function getFilesLength() public view returns (uint256) {
+    function getFilesCount() public view returns (uint256) {
         return _ownerToFiles[msg.sender].length;
     }
 
@@ -107,7 +105,7 @@ contract Storage {
      * @dev Get only one file
      * @param _index Index of the file
      */
-    function getFile(uint256 _index) public view returns (File memory) {
+    function getSingleFile(uint256 _index) public view returns (File memory) {
         require(
             _index < _ownerToFiles[msg.sender].length,
             "Index out of bounds"
@@ -118,9 +116,41 @@ contract Storage {
     /**
      * @dev Get all files of the owner
      */
-    function getFiles() public view returns (File[] memory) {
+    function getAllFiles() public view returns (File[] memory) {
         require(_ownerToFiles[msg.sender].length > 0, "No files found!");
         return _ownerToFiles[msg.sender];
+    }
+
+    function min(uint256 first, uint256 last) private pure returns (uint256) {
+        if (first < last) {
+            return first;
+        } else {
+            return last;
+        }
+    }
+
+    function getRangeOfFiles(uint256 _startIndex, uint256 _endIndex)
+        public
+        view
+        returns (File[] memory)
+    {
+        require(_ownerToFiles[msg.sender].length > 0, "No files found!");
+        require(
+            _startIndex < _ownerToFiles[msg.sender].length,
+            "Index out of bounds"
+        );
+        require(
+            _endIndex > _startIndex,
+            "start index must be strictly smaller than the end index"
+        );
+
+        _endIndex = min(_endIndex, _ownerToFiles[msg.sender].length);
+        uint256 range = _endIndex - _startIndex;
+        File[] memory result = new File[](range);
+        for (uint256 i = _startIndex; i < _endIndex; ++i) {
+            result[i - _startIndex] = _ownerToFiles[msg.sender][i];
+        }
+        return result;
     }
 
     /**
@@ -128,7 +158,7 @@ contract Storage {
      * @param _index Index of the file
      * @param _to Address to share the file with
      */
-    function shareFile(address _to, uint256 _index) public returns (bool) {
+    function shareFile(address _to, uint256 _index) public {
         require(
             _index < _ownerToFiles[msg.sender].length,
             "Index out of bounds!"
@@ -138,25 +168,27 @@ contract Storage {
 
         File memory file = _ownerToFiles[msg.sender][_index];
 
-        _ownerToFiles[_to].push(file);
+        // check if the file's hash is already present in the hashes array of the receiver
+        if (_hashToIndex[_to][file.hash] == 0) {
+            _ownerToFiles[_to].push(file);
+            _hashToIndex[_to][file.hash] = _ownerToFiles[_to].length;
 
-        emit FileShared(
-            _to,
-            file.name,
-            file.size,
-            file.mimeType,
-            file.hash,
-            block.timestamp
-        );
-
-        return true;
+            emit FileShared(
+                _to,
+                file.name,
+                file.size,
+                file.mimeType,
+                file.hash,
+                block.timestamp
+            );
+        }
     }
 
     /**
      * @dev Remove a file
      * @param _index Index of the file
      */
-    function removeFile(uint256 _index) public returns (bool) {
+    function removeFile(uint256 _index) public {
         require(
             _index < _ownerToFiles[msg.sender].length,
             "Index out of bounds!"
@@ -167,6 +199,10 @@ contract Storage {
             _ownerToFiles[msg.sender].length - 1
         ];
         _ownerToFiles[msg.sender].pop();
+        _hashToIndex[msg.sender][
+            _ownerToFiles[msg.sender][_index].hash
+        ] = _hashToIndex[msg.sender][file.hash];
+        _hashToIndex[msg.sender][file.hash] = 0;
 
         emit FileRemoved(
             file.name,
@@ -175,7 +211,5 @@ contract Storage {
             file.hash,
             block.timestamp
         );
-
-        return true;
     }
 }
